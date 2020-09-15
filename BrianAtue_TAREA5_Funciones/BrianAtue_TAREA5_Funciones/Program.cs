3dd01ce6b491bba2c586dd43ef8acd5f4692c69b
Brian <brian.atue@outlook.com>
2020-09-15 18:35:13 -0300

    agregue carpeta de C#

---
+++ BrianAtue_TAREA5_Funciones/BrianAtue_TAREA5_Funciones/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BrianAtue_TAREA5_Funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! " + args[0]);
+        }
+    }
+}
