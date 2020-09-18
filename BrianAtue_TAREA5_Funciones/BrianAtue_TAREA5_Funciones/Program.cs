4842db2b06e1f4bfbb168123d709578bcd1fe6d1
Brian <brian.atue@outlook.com>
2020-09-18 19:11:48 -0300

    agregado archivos del cuestionario y word de ejercicionos

--- BrianAtue_TAREA5_Funciones/BrianAtue_TAREA5_Funciones/Program.cs
+++ BrianAtue_TAREA5_Funciones/BrianAtue_TAREA5_Funciones/Program.cs
@@ -6,7 +6,22 @@ namespace BrianAtue_TAREA5_Funciones
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! " + args[0]);
+            int num1, num2, resultado;
+            int suma(int numero1, int numero2)
+            {
+                int suma = numero1 + numero2;
+                return suma;
+            }
+
+            Console.Write("ingrese un numero: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("ingrese un numero: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            resultado = suma(num1, num2);
+
+            Console.WriteLine("el resultado de la suma es: " + resultado);
+
         }
     }
 }
